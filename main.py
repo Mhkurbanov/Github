2530046661438c486f726e2e0c8bb08b21a745c6
Mhkurbanov <mk4304146@gmail.com>
2024-04-07 16:48:48 +0300

    Добавили if __name__ и функцию minus.

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ def plus(a, b):
     return a+b
 
 
-print(plus(23, 46))
+text = "new function"
+def minus(a, b):
+    return a-b
+
+
+if __name__ == "__main__":
+    print(plus(23, 46))
+    print(minus(80, 45))
